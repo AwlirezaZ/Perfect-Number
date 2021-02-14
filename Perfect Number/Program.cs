2aa13bc45b17decccfeae0313ceb637c67569d14
AwlirezaZ <ahmadialireza317@gmail.com>
2021-02-15 01:06:39 +0330

    intial commit

---
+++ Perfect Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Perfect_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Number");
+            long Number = Convert.ToInt64(Console.ReadLine());
+            long sum = 0;
+            for ( long i = 1; i < Number; i++)
+            {
+                if ( Number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            if (sum == Number)
+            {
+                Console.WriteLine("It's a Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine("It is NOT a Perfect Number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
